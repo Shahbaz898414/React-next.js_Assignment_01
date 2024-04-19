90c2c68230efdb1b59d36b01e2512e7fd37c4fac
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-19 22:57:41 +0530

    first

---
+++ components/InputControl/Inputcontrol.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Input.module.css';
+
+function Inputcontrol({ label, ...props }) {
+  return (
+    <div className={style.container}>
+      {label && <label htmlFor="" className={style.label} id="main">{label}</label>} {/* Fix htmlFor attribute */}
+
+      <input type="text" {...props}  className="input"/>
+    </div>
+  );
+}
+
+export default Inputcontrol;
